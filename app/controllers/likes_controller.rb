2c76bc97183b3167c49b75da41658c809eeee14d
Huynh Kien Thanh <hkthanh89@gmail.com>
2013-03-14 04:54:35 +0700

    Add button Like/Unlike to ribbits index page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,12 +5,12 @@ class LikesController < ApplicationController
   def create
     @ribbit = Ribbit.find(params[:like][:ribbit_id])
     current_user.like!(@ribbit)
-    respond_with @ribbit
+    respond_with(@ribbit, location: ribbits_url)
   end
 
   def destroy
     @ribbit = Like.find(params[:id]).ribbit
     current_user.unlike!(@ribbit)
-    respond_with @ribbit
+    respond_with(@ribbit, location: ribbits_url)
   end
 end
